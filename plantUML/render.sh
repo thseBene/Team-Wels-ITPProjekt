5ae0273614f913046fa31351c63efbc40c433090
thseBene <b.lehner1@students.htl-leonding.ac.at>
2025-11-05 13:55:31 +0100

    update render.sh

--- plantUML/render.sh
+++ plantUML/render.sh
@@ -1,33 +1,19 @@
-# Verzeichnis erstellen
-New-Item -ItemType Directory -Force -Path "public" | Out-Null
+#!/usr/bin/env bash
+# (c) Aberger (2025) https://www.aberger.at
 
-# Alle .puml-Dateien in den Ordner kopieren
-Copy-Item -Path "*.puml" -Destination "public" -Force
+## render a state diagram with plant-uml docker
 
-# In das Verzeichnis wechseln
-Push-Location "public"
+mkdir -p public
+cp *.puml public
+pushd public
+    rm -f *.png *.svg
+    echo "PWD is $(pwd)"
 
-# Alte .png und .svg löschen
-Remove-Item -Path "*.png", "*.svg" -Force -ErrorAction SilentlyContinue
-
-# Aktuelles Verzeichnis ausgeben
-Write-Host "PWD is $(Get-Location)"
-
-# PlantUML via Docker ausführen und SVG erzeugen
-Get-Content "state.puml" | docker run --rm -i --entrypoint java ghcr.io/plantuml/plantuml -jar "/opt/plantuml.jar" -tsvg -p | Set-Content "state.svg"
-
-# Aktuelles Verzeichnis ausgeben und Inhalt listen
-Get-Location
-Get-ChildItem -Force
-
-# Optional: PDF generieren (auskommentiert)
-# Get-Content "state.puml" | docker run --rm -i --entrypoint java ghcr.io/plantuml/plantuml -jar "/opt/plantuml.jar" -tpdf -p | Set-Content "state.pdf"
-
-# Sonderzeichen-Dateien und .puml löschen
-Remove-Item -Path '?' -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item -Path "*.puml" -Force -ErrorAction SilentlyContinue
-
-# Zurück ins ursprüngliche Verzeichnis
-Pop-Location
-
+    cat state.puml | docker container run --rm -i --entrypoint java ghcr.io/plantuml/plantuml -jar "/opt/plantuml.jar" -tsvg -p > state.svg
+    pwd
+    ls -l .
+    #cat state.puml | docker container run --rm -i --entrypoint java ghcr.io/plantuml/plantuml -jar "/opt/plantuml.jar" -tpdf -p > state.pdf
+    rm -rf '?'
+    rm *.puml
+popd
 
