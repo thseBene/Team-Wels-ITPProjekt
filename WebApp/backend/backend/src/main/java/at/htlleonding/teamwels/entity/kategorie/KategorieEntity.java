8644db02307c1a327c1cdd3ad71c6a8f770f00ce
SimonDokter <s.dokter@students.htl-leonding.ac.at>
2026-01-14 13:43:36 +0100

    merge fixes

--- WebApp/backend/backend/src/main/java/at/htlleonding/teamwels/entity/kategorie/KategorieEntity.java
+++ WebApp/backend/backend/src/main/java/at/htlleonding/teamwels/entity/kategorie/KategorieEntity.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package at.htlleonding.teamwels.entity.kategorie;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -16,25 +15,4 @@ public class KategorieEntity extends PanacheEntity {
     public String bezeichnung;
 
 
-=======
-package at.htlleonding.teamwels.entity.kategorie;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Table(name = "kategorie")
-public class KategorieEntity extends PanacheEntity {
-
-    @Column(name = "bezeichnung", nullable = false)
-    public String bezeichnung;
-
-    @ManyToMany(mappedBy = "kategorien")
-    @JsonIgnore
-    public List<at.htlleonding.teamwels.entity.feedback.FeedbackEntity> feedbacks = new ArrayList<>();
->>>>>>> Stashed changes
 }
