4557bd3475c4e8c0c25fb4282e1dbd50c0c314cd
JakobPeneder <j.peneder@students.htl-leonding.ac.at>
2026-01-21 13:31:40 +0100

    #81 working on the LogSystem

---
+++ WebApp/frontend/public/dist/employeeDashboard.js
@@ -0,0 +1,39 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { getAllFeedback, getLogSystem } from "./api/feedback-api.js";
+import "./components/FeedbackView.js";
+function renderFeedbackList() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const container = document.getElementById("container");
+        if (!container)
+            return;
+        const feedbackList = yield getAllFeedback();
+        container.innerHTML = ""; // leeren
+        feedbackList.forEach(fb => {
+            const feedbackEl = document.createElement("feedback-view");
+            feedbackEl.feedbackId = fb.id;
+            feedbackEl.subject = fb.subject;
+            feedbackEl.status = fb.status;
+            container.appendChild(feedbackEl);
+        });
+    });
+}
+// initial render
+// renderFeedbackList();
+// logging System
+function logFeedbackSystem() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const container = document.getElementById("container");
+        if (!container)
+            return;
+        const logList = yield getLogSystem();
+    });
+}
+logFeedbackSystem();
--- WebApp/frontend/public/src/api/feedback-api.ts
+++ WebApp/frontend/public/src/api/feedback-api.ts
@@ -3,10 +3,11 @@ export interface Feedback {
     subject: string;
     status: string;
 }
+const baseUrl = "http://localhost:8080";
 
 // Holen aller Feedbacks
 export async function getAllFeedback(): Promise<Feedback[]> {
-    const res = await fetch("http://localhost:8080/api/feedback");
+    const res = await fetch(`${baseUrl}/api/feedback`);
     console.log(res);
     if (!res.ok) throw new Error(`Fehler beim Abrufen: ${res.status}`);
     return res.json();
@@ -14,7 +15,7 @@ export async function getAllFeedback(): Promise<Feedback[]> {
 
 // Update Status
 export async function updateByID(id: number, newStatus: string): Promise<void> {
-    const res = await fetch(`http://localhost:8080/api/feedback/${id}/status`, {
+    const res = await fetch(`${baseUrl}/api/feedback/${id}/status`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ status: newStatus })
@@ -25,14 +26,14 @@ export async function updateByID(id: number, newStatus: string): Promise<void> {
 
 // Delete
 export async function deleteByID(id: number): Promise<void> {
-    const res = await fetch(`http://localhost:8080/api/feedback/${id}`, { method: "DELETE" });
+    const res = await fetch(`${baseUrl}/api/feedback/${id}`, { method: "DELETE" });
     console.log(res);
     if (!res.ok) throw new Error(`Fehler beim Löschen: ${res.status}`);
 }
 
 
 export async function employeeLogin(benutzername: string, passwort: string): Promise<void> {
-    const res = await fetch('http://localhost:8080/api/auth/login', {
+    const res = await fetch(`${baseUrl}/api/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -45,5 +46,18 @@ export async function employeeLogin(benutzername: string, passwort: string): Pro
     
     const data = await res.json();
     console.log('Daten ', data);
+}
+export async function getLogSystem() {
+    const res = await fetch(`${baseUrl}/api/activityLog`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 
+    console.log(res);
+    if (!res.ok) throw new Error(`Fehler beim Abrufen der Logs: ${res.status}`);
+    
+    const data = await res.json();
+    console.log('Logs ', data);
 }
